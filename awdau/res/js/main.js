e678998ccc0e47c67dd3d2d56c24372a771718fc
Štěpán Tomsa <115143064+Tomsatomsic@users.noreply.github.com>
2023-02-19 23:03:51 +0100

    Add files via upload

---
+++ awdau/res/js/main.js
@@ -0,0 +1,9 @@
+var donut = document.getElementById("donut")
+var restaurant = document.getElementById("restaurant")
+
+
+
+function Scene() { 
+donut.style.display = "none";
+restaurant.style.display = "flex";
+}
