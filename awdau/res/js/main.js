b4210660a88620729f9255c29b8599d62210670f
Štěpán Tomsa <115143064+Tomsatomsic@users.noreply.github.com>
2023-02-20 10:14:02 +0100

    Update main.js

--- awdau/res/js/main.js
+++ awdau/res/js/main.js
@@ -1,9 +1,16 @@
 var donut = document.getElementById("donut")
 var restaurant = document.getElementById("restaurant")
-
+var bubble = document.getElementById("bubble")
 
 
 function Scene() { 
 donut.style.display = "none";
 restaurant.style.display = "flex";
+welcome.style.display = "flex";
 }
+
+function Scene2(){
+    restaurant.style.display = "none";
+    inside.style.display = "flex";
+}
+
