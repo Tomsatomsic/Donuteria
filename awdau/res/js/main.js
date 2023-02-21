fd87313af609a6580e21c9176fac01d643df51f2
Štěpán Tomsa <115143064+Tomsatomsic@users.noreply.github.com>
2023-02-21 23:13:21 +0100

    Update main.js

--- awdau/res/js/main.js
+++ awdau/res/js/main.js
@@ -1,23 +1,35 @@
 var donut = document.getElementById("donut")
 var restaurant = document.getElementById("restaurant")
-var bubble = document.getElementById("bubble")
 var inside = document.getElementById("inside")
 var kitchen = document.getElementById("kitchen")
 var customerlist = document.getElementById("customers")
-
-
+var welcome = document.getElementById("welcome")
+var click = document.getElementById("click")
+var guss = document.getElementById("guss")
+var kuchyn = document.getElementById("kuchyn")
 
 
 
 function Scene() { 
 donut.style.display = "none";
 restaurant.style.display = "flex";
-welcome.style.display = "flex";
+click.style.display = "flex";
 }
+
 
 function Scene2(){
-    restaurant.style.display = "none";
+    click.style.display = "none";
+    restaurant.style.display="none";
     inside.style.display = "flex";
     guss.style.display = "flex";
+    welcome.style.display = "flex";
+    kuchyn.style.display = "flex";
 }
 
+function Scene3(){
+    kuchyn.style.display = "none";
+    welcome.style.display = "none";
+    inside.style.display = "none";
+    guss.style.display = "none";
+    kitchen.style.display = "flex";
+}
