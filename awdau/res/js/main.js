ec229ca0d950699075eea8840a027825108c7e26
Štěpán Tomsa <115143064+Tomsatomsic@users.noreply.github.com>
2023-02-21 11:16:36 +0100

    Update main.js

--- awdau/res/js/main.js
+++ awdau/res/js/main.js
@@ -1,6 +1,12 @@
 var donut = document.getElementById("donut")
 var restaurant = document.getElementById("restaurant")
 var bubble = document.getElementById("bubble")
+var inside = document.getElementById("inside")
+var kitchen = document.getElementById("kitchen")
+var customerlist = document.getElementById("customers")
+
+
+
 
 
 function Scene() { 
@@ -12,5 +18,6 @@ welcome.style.display = "flex";
 function Scene2(){
     restaurant.style.display = "none";
     inside.style.display = "flex";
+    guss.style.display = "flex";
 }
 
