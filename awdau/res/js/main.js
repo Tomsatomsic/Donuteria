b5c7834db7912dad766981ab66c6a3d3c12888d7
Štěpán Tomsa <115143064+Tomsatomsic@users.noreply.github.com>
2023-03-03 15:35:52 +0100

    Update main.js

--- awdau/res/js/main.js
+++ awdau/res/js/main.js
@@ -10,8 +10,18 @@ var kuchyn = document.getElementById("kuchyn")
 var back = document.getElementById("back")
 var toilets = document.getElementById("toilets")
 var toilet = document.getElementById("toilet")
+var doughnut = document.getElementById("doughnut")
+var candystar = document.getElementById("candystar")
+var sprinkles = document.getElementById("sprinkles")
+var nutella = document.getElementById("nutella")
+var marmelade = document.getElementById("marmelade")
 
 
+let doughnuts = 0;
+let sprinkle = 0;
+let candystars = 0;
+let nutellas = 0;
+let marmelades = 0;
 
 function Scene() { 
 donut.style.display = "none";
@@ -21,6 +31,7 @@ click.style.display = "flex";
 
 
 function Scene2(){
+    toilets.style.display = "flex"; 
     inside.style.display = "flex";
     guss.style.display = "flex";
     welcome.style.display = "flex";
@@ -30,12 +41,18 @@ function Scene2(){
 }
 
 function Scene3(){
+    marmelade.style.display = "flex";
+    candystar.style.display = "flex";
+    nutella.style.display = "flex";
+    sprinkles.style.display = "flex";
+    doughnut.style.display = "flex";
     kitchen.style.display = "flex";
     back.style.display = "flex";
     kuchyn.style.display = "none";
     welcome.style.display = "none";
     inside.style.display = "none";
     guss.style.display = "none";
+    toilets.style.display = "none";
 }
 
 function Back(){
@@ -50,8 +67,13 @@ kitchen.style.display = "none";
 function Scene4(){
     toilet.style.display = "flex";
     back.style.display = "flex";
+    toilets.style.display = "none";
     kuchyn.style.display = "none";
     welcome.style.display = "none";
     inside.style.display = "none";
     guss.style.display = "none";
+}
+
+function dugnut() {
+doughnuts++;
 }
